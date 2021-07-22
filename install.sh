408ef9f6681be1edee310a2b2aafe29a86524818
riliumph <riliumph@gmail.com>
2021-07-22 16:59:50 +0900

    [Add] install shell

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/sh
+brew update
+brew install docker
+brew install exa
+brew install ffmpeg
+brew install fish
+brew install git
+brew install htop
+brew install imagemagick
+brew install jq
+brew install peco
+brew install pyenv
+brew install wget
+brew install --cask appcleaner
+brew install --cask bettertouchtool
+#brew install --cask eul
+brew install --cask firefox
+brew install --cask google-backup-and-sync
+brew install --cask google-chrome
+brew install --cask google-japanese-ime
+brew install --cask hyperswitch
+brew install --cask iterm2
+brew install --cask karabiner-elements
+brew install --cask macvim
+brew install --cask qblocker
+brew install --cask rectangle
+brew install --cask steam 
+brew install --cask visual-studio-code
+brew install --cask vlc
